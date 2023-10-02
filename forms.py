284c7cc4336c7f769af399976dea95dc89656c9f
Ignacy Berent <2gb02ignac@gmail.com>
2023-10-02 11:16:02 +0200

    tabelowy podgląd dostępności

--- forms.py
+++ forms.py
@@ -1,23 +1,40 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, PasswordField, FloatField, IntegerField, SelectMultipleField, FieldList, FormField, TimeField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
 from flask_ckeditor import CKEditorField
 
 
 # WTForm for creating a blog post
-class CreatePostForm(FlaskForm):
-    subject = StringField("Blog Post Title", validators=[DataRequired()])
-    hours_per_lesson = StringField("Subtitle", validators=[DataRequired()])
-    lessons_per_week = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    weekday = CKEditorField("Blog Content", validators=[DataRequired()])
-    hours = SubmitField("Submit Post")
+class AvailabilityForm(FlaskForm):
+    availabilities = FieldList(StringField('Dostępność'), label='Dostępność', min_entries=7*16)
+    submit = SubmitField('Dodaj Post')
+
+
+class TutoringPostForm(FlaskForm):
+    subject = StringField('Przedmiot', validators=[DataRequired()])
+    hours_per_lesson = FloatField('Godziny na lekcję', validators=[DataRequired()])
+    lessons_per_week = IntegerField('Lekcje na tydzień', validators=[DataRequired()])
+    # Tutaj mamy listę formularzy dostępności dla każdego postu
+    availabilities = FieldList(FormField(AvailabilityForm), label='Dostępność')
+    submit = SubmitField('Dodaj Post')
 
 
 
 # Create a RegisterForm to register new users
 class UczenRegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=8, message="Hasło musi mieć co najmniej 8 znaków."),
+            Regexp(r'(?=.*\d)', message="Hasło musi zawierać co najmniej jedną cyfrę."),
+            Regexp(r'(?=.*[A-Z])', message="Hasło musi zawierać co najmniej jedną wielką literę."),
+            Regexp(r'(?=.*[a-z])', message="Hasło musi zawierać co najmniej jedną małą literę."),
+            Regexp(r'(?=.*[!@#$%^&*])', message="Hasło musi zawierać co najmniej jeden ze znaków specjalnych (!@#$%^&*).")
+        ]
+    )
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
     name = StringField("Imię", validators=[DataRequired()])
     surname = StringField("Nazwisko", validators=[DataRequired()])
     phone_number = StringField("Numer telefonu", validators=[DataRequired()])
@@ -25,8 +42,19 @@ class UczenRegisterForm(FlaskForm):
 
 
 class KorepetytorRegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email(message="Nieprawidłowy format adresu email.")])
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=8, message="Hasło musi mieć co najmniej 8 znaków."),
+            Regexp(r'(?=.*\d)', message="Hasło musi zawierać co najmniej jedną cyfrę."),
+            Regexp(r'(?=.*[A-Z])', message="Hasło musi zawierać co najmniej jedną wielką literę."),
+            Regexp(r'(?=.*[a-z])', message="Hasło musi zawierać co najmniej jedną małą literę."),
+            Regexp(r'(?=.*[!@#$%^&*])', message="Hasło musi zawierać co najmniej jeden ze znaków specjalnych (!@#$%^&*).")
+        ]
+    )
+    confirm_password = PasswordField("Potwierdź hasło", validators=[DataRequired(), EqualTo('password', message='Hasła muszą się zgadzać.')])
     name = StringField("Imię", validators=[DataRequired()])
     surname = StringField("Nazwisko", validators=[DataRequired()])
     phone_number = StringField("Numer telefonu", validators=[DataRequired()])
@@ -41,7 +69,8 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Let Me In!")
 
 
-# Create a CommentForm so users can leave comments below posts
-class CommentForm(FlaskForm):
-    comment = CKEditorField("Comment")
-    submit = SubmitField("Send comment")
+# Create a confirm form to confirm users
+class ConfirmEmailForm(FlaskForm):
+    confirmation_code = StringField("Confirmation Code:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
